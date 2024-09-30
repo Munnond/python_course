25689f365deb30c49d55a1b2ed129f24620f4561
nd2812 <nisarg2005prajapati@gmail.com>
2024-09-30 21:38:58 +0530

    2nd commit

---
+++ D5/main.py
@@ -0,0 +1,15 @@
+'''
+This is multi line comment
+hi
+hi
+hi
+'''
+
+
+# hey Harry , Please dont remove this line
+print("Hey I am a \"good boy\"\nand this viewer is also a good boy/girl")
+
+# print("Hello World") #printing Hello World
+
+print("Hey",6,7,sep="~",end="009\n")
+print("Harry")
---
+++ D6/main.py
@@ -0,0 +1,8 @@
+a=1
+print(a)
+b="Harry"
+print(b)
+print("The type of a is ",type(a))
+
+list1 = [8,2.3,[-4,5],["apple","banana"]]
+print(list1)
